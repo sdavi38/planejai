3e0fb0259275a771d64a7a5a65d3e7483a35948a
sdavi38 <sdavi2840@gmail.com>
2026-06-14 15:03:22 -0300

    feat: add Content and Error components to the insights feature module

--- src/pages/components/features/SimulationResult/AlInsightCardProps.tsx
+++ src/pages/components/features/SimulationResult/AlInsightCardProps.tsx
@@ -1,16 +1,28 @@
 import { useInsight } from "../../../../hooks/useInsight"
-
+import { Content } from "../insights/Content"
+import { Error } from "../insights/Error"
 
 interface AIInsightCardProps {
     simulationId: string
 }
 export function AIInsightCard({ simulationId }: AIInsightCardProps) {
-    const { insight } = useInsight(simulationId)
-    console.log(insight)
+    const { insight, isLoading, error, fetchInsigh } = useInsight(simulationId)
 
     return (
         <div className="bg-card order-2 rounded-2xl lg:order-1 lg:col-span-2 p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)]">
-            painel insigth
+            <div className="mb-3 flex items-center gap-1.5">
+                <span>✨</span>
+                <span className="text-primary text-xs font-semibold tracking-widest uppercase">
+                    Insight Financeiro Personalizado
+                </span>
+
+            </div>
+            {isLoading && <p>Gerando seus insights...</p>}
+            {!isLoading && error && (
+                <Error simulationId={simulationId} message={error}
+                    onRetry={() => { fetchInsigh(simulationId) }} />
+            )}
+            {!isLoading && insight && error && <Content />}
         </div>
     )
 }
